01af378d8fa4613c52c4988a5c94ba1d092de0fb
Indra Yudiana <154766195+ydxnidr@users.noreply.github.com>
2024-10-23 00:31:42 +0700

    add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './application/views/**/*.php',
+        './application/views/**/*.html'
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
+
