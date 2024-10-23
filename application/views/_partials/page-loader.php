8078f4fa126f6b7286c153f0378372c94b922689
Indra Yudiana <154766195+ydxnidr@users.noreply.github.com>
2024-10-23 21:26:22 +0700

    create page-loader.php

---
+++ application/views/_partials/page-loader.php
@@ -0,0 +1,13 @@
+<div class="fixed top-0 right-0 h-screen w-screen bg-white bg-opacity-100 z-50 flex items-center justify-center flex-col" id="loading" style="opacity: 0; transition: opacity 0.5s ease-in-out;">
+    <i class="ki-outline ki-arrows-circle animate-spin bg-gradient-to-r from-blue-500 to-cyan-500 inline-block text-transparent bg-clip-text text-[35px]"></i>
+    <span class="text-[15px] text-semibold text-gray-400 mt-[5px]">Memuat...</span>
+</div>
+<script>
+    document.getElementById("loading").style.opacity = "1";
+    setTimeout(() => {
+        document.getElementById("loading").style.opacity = "0";
+        setTimeout(() => {
+            document.getElementById("loading").classList.add("hidden");
+        }, 300);
+    }, 150);
+</script>
