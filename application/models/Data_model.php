882dad033838352b90ed4d4ba9640101e88ab3ac
Indra Yudiana <154766195+ydxnidr@users.noreply.github.com>
2024-10-23 21:23:55 +0700

    create Data_model.php and User_model.php

---
+++ application/models/Data_model.php
@@ -0,0 +1,40 @@
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed');
+                        
+class Data_model extends CI_Model 
+{
+    // fungsi untuk mengambil data dari database
+    function get_data($table)
+    {
+        return $this->db->get($table);
+    }
+    
+    // fungsi untuk menginput data ke database
+    function insert_data($data, $table)
+    {
+        $this->db->insert($table, $data);
+    }
+    
+    // fungsi untuk mengedit data
+    function edit_data($where, $table)
+    {
+        return $this->db->get_where($table, $where);
+    }
+
+    // fungsi untuk mengupdate atau mengubah data di database
+    function update_data($where, $data, $table)
+    {
+        $this->db->where($where);
+        $this->db->update($table, $data);
+    }
+
+    // fungsi untuk menghapus data dari database
+    function delete_data($where, $table)
+    {
+        $this->db->delete($table, $where);
+    }                   
+                        
+}
+
+
+/* End of file Data_model.php and path \application\models\Data_model.php */
---
+++ application/models/User_model.php
@@ -0,0 +1,14 @@
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed');
+                        
+class User_model extends CI_Model 
+{
+    public function cek_login($where = null)
+    {
+        return $this->db->get_where('users', $where);
+    }                    
+                        
+}
+
+
+/* End of file User_model.php and path \application\models\User_model.php */
